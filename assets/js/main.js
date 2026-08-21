3b612e461fcc325c9898233c961b61ee87d8b481
bgstudio181 <your-email@example.com>
2026-08-21 09:29:02 +0100

    update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,35 @@
-let p = document.querySelector("p")
-console.log(p)
-body.innerHTML="88"
+let left1=document.querySelector("#left1")
+let right1=document.querySelector("#right1")
+let scrol=document.querySelector(".scrol")
+let scrol_position=0
+left1.addEventListener("click" ,()=>{
+  if (scrol_position!=1000){
+    scrol_position+=100
+    scrol.style.transform += "translateX(-100px)"
+  }
+})
+right1.addEventListener("click" ,()=>{
+  if (scrol_position!=0){
+    scrol_position-=100
+    scrol.style.transform += "translateX(100px)"
+  }
+})
+
+let left2=document.querySelector("#left2")
+let right2=document.querySelector("#right2")
+let scrol2=document.querySelector(".scrol1")
+let scrol2_position=0
+left2.addEventListener("click" ,()=>{
+  if (scrol2_position!=1000){
+    scrol2_position+=100
+    scrol2.style.transform += "translateX(-100px)"
+  }
+})
+
+right2.addEventListener("click" ,()=>{
+  if (scrol2_position!=0){
+    scrol2_position-=100
+    scrol2.style.transform += "translateX(100px)"
+  }
+})
+//*5
